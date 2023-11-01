1df77841123591b80cfdfbfd64aebc31b8414967
William Zhou <william.zhou@duke.edu>
2023-11-01 16:56:22 -0400

    Changed preload

--- components/animation.tsx
+++ components/animation.tsx
@@ -90,46 +90,6 @@ const StartAnimation = ({ time }: StartProps) => {
       zIndex="5"
       bgColor="white"
     >
-      <Image
-        position="fixed"
-        src="layer0.jpeg"
-        alt="background layer"
-        zIndex="-3"
-        loading="eager"
-        opacity="0"
-      ></Image>
-      <Image
-        position="fixed"
-        src="layer1.png"
-        alt="back mountains"
-        zIndex="-3"
-        loading="eager"
-        opacity="0"
-      ></Image>
-      <Image
-        position="fixed"
-        src="middlerock.png"
-        alt="middle rock"
-        zIndex="-3"
-        loading="eager"
-        opacity="0"
-      ></Image>
-      <Image
-        position="fixed"
-        src="layer2.png"
-        alt="back right rock"
-        zIndex="-3"
-        loading="eager"
-        opacity="0"
-      ></Image>
-      <Image
-        position="fixed"
-        src="layer3.png"
-        alt="close rock"
-        zIndex="-3"
-        loading="eager"
-        opacity="0"
-      ></Image>
       {time <= 2 && (
         <Flex align="center" justify="center" h="100vh">
           <Flex align="center" justify="center">
@@ -234,6 +194,46 @@ const StartAnimation = ({ time }: StartProps) => {
               </Box>
             </Box>
           </Flex>
+          <Image
+            position="fixed"
+            src="layer0.jpeg"
+            alt="background layer"
+            zIndex="-3"
+            loading="eager"
+            opacity="0"
+          ></Image>
+          <Image
+            position="fixed"
+            src="layer1.png"
+            alt="back mountains"
+            zIndex="-3"
+            loading="eager"
+            opacity="0"
+          ></Image>
+          <Image
+            position="fixed"
+            src="middlerock.png"
+            alt="middle rock"
+            zIndex="-3"
+            loading="eager"
+            opacity="0"
+          ></Image>
+          <Image
+            position="fixed"
+            src="layer2.png"
+            alt="back right rock"
+            zIndex="-3"
+            loading="eager"
+            opacity="0"
+          ></Image>
+          <Image
+            position="fixed"
+            src="layer3.png"
+            alt="close rock"
+            zIndex="-3"
+            loading="eager"
+            opacity="0"
+          ></Image>
         </Flex>
       )}
     </Box>
